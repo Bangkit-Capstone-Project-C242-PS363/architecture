f28383cd373fd005cc4ed2f32e749416ca5876a2
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-11-14 13:48:03 +0700

    enable compute api

--- main.tf
+++ main.tf
@@ -2,3 +2,19 @@ provider "google" {
   project = var.project_name
   region  = var.region
 }
+
+locals {
+  services = [
+    "compute.googleapis.com",
+  ]
+}
+
+resource "google_project_service" "project_services" {
+  for_each = toset(local.services)
+
+  project = var.project_name
+  service = each.value
+
+  disable_dependent_services = true
+  disable_on_destroy         = true
+}
