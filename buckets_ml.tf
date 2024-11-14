b0e3d7e5b1fc90ddcd224f75a986bd2446a0e44b
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-11-14 14:20:20 +0700

    delete dataset bucket: ineffective

--- buckets_ml.tf
+++ buckets_ml.tf
@@ -1,9 +1,3 @@
-variable "bucket_dataset" {
-  description = "The name of the bucket to store the dataset"
-  type        = string
-  default     = "bucket-asl-dataset"
-}
-
 variable "bucket_model" {
   description = "The name of the bucket to store the dataset"
   type        = string
@@ -14,21 +8,6 @@ variable "ml_dev_emails" {
   description = "List of email addresses to grant bucket access"
   type        = list(string)
   default     = []
-}
-
-resource "google_storage_bucket" "bucket_asl_dataset" {
-  name          = var.bucket_dataset
-  force_destroy = false # Prevent accidental deletion of the bucket
-  location      = var.region
-
-  lifecycle_rule {
-    condition {
-      age = 360
-    }
-    action {
-      type = "Delete"
-    }
-  }
 }
 
 resource "google_storage_bucket" "bucket_asl_model" {
@@ -44,14 +23,6 @@ resource "google_storage_bucket" "bucket_asl_model" {
       type = "Delete"
     }
   }
-}
-
-resource "google_storage_bucket_iam_member" "dataset_bucket_admin" {
-  for_each = toset(var.ml_dev_emails)
-
-  bucket = google_storage_bucket.bucket_asl_dataset.name
-  role   = "roles/storage.admin"
-  member = "user:${each.value}"
 }
 
 resource "google_storage_bucket_iam_member" "model_bucket_admin" {
